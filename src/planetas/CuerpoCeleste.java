36e76a0a511ab836b0dfe44590f3e801a52320a5
Marcos <Marcos@Profesor>
2024-03-18 08:47:18 +0100

    Proyecto creado con la clase abstracta CuerpoCeleste

---
+++ src/planetas/CuerpoCeleste.java
@@ -0,0 +1,42 @@
+package planetas;
+
+public abstract class  CuerpoCeleste {
+
+	
+	private String nombre;
+	private double masa;	
+	
+	
+	public CuerpoCeleste(String nombre, double masa) {
+		super();
+		this.nombre = nombre;
+		this.masa = masa;
+	}
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getMasa() {
+		return masa;
+	}
+	public void setMasa(double masa) {
+		this.masa = masa;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "CuerpoCeleste [" + nombre + " (" + masa + ")]";
+	}
+
+	
+	
+	
+	
+	
+	
+}
